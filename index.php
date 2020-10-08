a902cbfddc9b3fc10a78d8e711d2f7152a675b5c
Arturo <arturo.goca02@gmail.com>
2020-10-07 23:07:49 -0500

    Test

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 <body>
     <nav class="navbar navbar-dark bg-dark navbar navbar-expand-lg">
-        <a class="navbar-brand" href="#">Agogoca</a>
+        <a class="navbar-brand" href="#">Agogoc</a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
